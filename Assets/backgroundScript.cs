bc65e330ea345cfc95200ffe10b03124a6625500
weilguny50 <weilguny50@mail.com>
2026-07-26 01:19:26 +0200

    kackschwein

---
+++ Assets/backgroundScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class backgroundScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public SpriteRenderer kackendesSchwein;
+    void Start()
+    {
+      kackendesSchwein = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(kackendesSchwein.flipX==true){
+        kackendesSchwein.flipX=false;}else{kackendesSchwein.flipX=true;}
+    }
+}
